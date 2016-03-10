847c860182dc91e1d34a8da57532ddee270239d6
edmilhomem <edeilson.milhomem@gmail.com>
2016-03-10 16:12:04 -0300

    Delete form1.php

--- php/formularios/metodo-post/form1.php
+++
@@ -1,8 +0,0 @@
-<html>
-<body>
-    <form name="form1" method="POST" action="mostrar-dados.php">
-        <input name="textNome" type="text">
-        <input name="btnEnviar" type="submit" value="Enviar">
-    </form>
-</body>
-</html>
