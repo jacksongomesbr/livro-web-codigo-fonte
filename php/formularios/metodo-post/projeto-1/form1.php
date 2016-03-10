fc84666caf9655e33e8cef29ffa4d346b6ceaddc
edmilhomem <edeilson.milhomem@gmail.com>
2016-03-10 16:13:56 -0300

    Create form1.php

---
+++ php/formularios/metodo-post/projeto-1/form1.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+    <form name="form1" method="POST" action="mostrar-dados.php">
+        <input name="textNome" type="text">
+        <input name="btnEnviar" type="submit" value="Enviar">
+    </form>
+</body>
+</html>
