b4866d45316f43465f9a7d6fd0ff84cccc5ffcc8
edmilhomem <edeilson.milhomem@gmail.com>
2016-03-21 11:09:52 -0300

    Create login.php

---
+++ php/sessao/projeto1/login.php
@@ -0,0 +1,11 @@
+<?php
+if ($_POST["textLogin"]=="user" && $_POST["textSenha"]="password" ){
+    session_start();
+    date_default_timezone_set("America/Sao_Paulo");
+    $_SESSION["user_name"]= $_POST["textLogin"];
+    $_SESSION["login_time"] = date("d-m-Y h:i:sa");
+    header("Location: profile.php");
+}else{
+    header('Location: frmLogin.php?erroLogin=1');
+}
+?>
